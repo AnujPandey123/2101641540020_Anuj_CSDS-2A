2feb7cd07ed1b154afe89baf0c3ea13eeec50d78
AnujPandey123 <120494847+AnujPandey123@users.noreply.github.com>
2022-12-13 22:48:03 +0530

    Create print.c

---
+++ print.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+typedef struct node{
+	int data;
+	struct node* address;
+};
+void insert(int data1,struct node* head){
+	struct node* temp=(struct node*)malloc(sizeof(struct node));
+	struct node* s;
+	temp->data=data1;
+	temp->address=NULL;
+	if(head==NULL){
+		head=temp;
+	}
+	else{
+		s=head;
+		while(s->next!=NULL){
+			s=s->address;
+		}
+		s->address=temp;
+	}
+}
+void print(head){
+	struct node* s;
+	s=head;
+	while(s!=NULL){
+		printf("%d",s->data);
+		printf("->");
+		s=s->address;
+	}
+}
+int main(){
+	struct node* head=NULL;
+	insert(10,&head);
+	print(head);
+	insert(100,&head);
+	insert(5,&head);
+	print(head);
+	
+}
